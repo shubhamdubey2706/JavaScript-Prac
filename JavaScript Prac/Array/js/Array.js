814b919028b514b857bf823cf85f54ec5980f856
Shubham Dubey <87197585+shubhamdubey2706@users.noreply.github.com>
2021-10-31 10:57:54 +0530

    Add files via upload

---
+++ JavaScript Prac/Array/js/Array.js
@@ -0,0 +1,20 @@
+//Array : array is special varial and we can hold multiple vaules at a time
+//for ex : we have multile car so we can define one variable and can hold multiple vaules
+//we can get any vaules by his reference or index number
+
+//let j = [34, 54, 67, 97, 87];
+//x = ["shubham", "rachit", "ram"];
+//console.log(x);
+//console.log(x[2]); // we can get index no
+//x[2] = "shyam";
+//console.log(x);
+
+//console.log(j);
+//o = j.indexOf(97);
+//console.log(o);  we can 97th    ssss index
+
+const x = [10, 30, 55, 45, 67];
+//console.log(x.length); // we can find lenght
+//console.log(x[2]); // we can get 2nd element
+const y = x.indexOf(30);
+console.log(y);
